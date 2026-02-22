0989f85b62508cf646d27fb7466e8c4ea334e3f1
Arun <youremail@example.com>
2026-02-23 00:43:21 +0530

    app.jsx update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
-import Login from './pages/Login.jsx'
-import Register from './pages/Register.jsx'
-import Home from './pages/Home.jsx';
-import Footer from './components/Footer.jsx';
-import Header from './components/Header.jsx';
+
+import Header from "./components/Common/Header";
+import Footer from "./components/Common/Footer";
+import AppRoutes from "./Routes/approutes";
 
 const App = () => {
   return (
-    <>
       <BrowserRouter>
         <Toaster />
-        <Header/>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-        <Footer/>
+      <Header />
+
+      <AppRoutes />
+
+      <Footer />
       </BrowserRouter>
-    
-    </>
-  )
-}
+  );
+};
 
 export default App;
